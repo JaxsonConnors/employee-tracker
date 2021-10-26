7c0da24b8a202cf9ca19937e66870d57696a09d8
Jaxson Connors <connorsjaxson@gmail.com>
2021-10-25 21:35:27 -0600

    added connection.js and options to view all 3 tables

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const inquirer = require('inquirer');
 const consoleTable = require('console.table');
+const connection = require('./connection');
+
+
+//Run App
+connection.connect((error) => {
+    if (error) throw error;
+    console.log('Employee Tracker');
+    promptUser();
+});
+
 
 const promptUser = () => {
     inquirer.prompt([
@@ -51,4 +61,48 @@ const promptUser = () => {
     });
 };
 
-promptUser();
+// View All Departments
+const viewAllDepartments = () => {
+    const sql =   `SELECT department.id AS id, department.department_name AS department FROM department`; 
+    connection.query(sql, (error, response) => {
+      if (error) throw error;
+      console.log(`All Departments:`);
+      console.table(response);
+      promptUser();
+    });
+};
+
+// View All Roles
+const viewAllRoles = () => {
+  console.log(`Current Employee Roles:`);
+  const sql =
+    `SELECT role.id, role.title, department.department_name AS department
+    FROM role
+    INNER JOIN department ON role.department_id = department.id`;
+  connection.query(sql, (error, response) => {
+    if (error) throw error;
+      response.forEach((role) => {console.log(role.title);});
+      promptUser();
+  });
+};
+
+// View All Employees
+const viewAllEmployees = () => {
+  let sql = 
+    `SELECT employee.id, 
+    employee.first_name, 
+    employee.last_name, 
+    role.title, 
+    department.department_name AS 'department', 
+    role.salary
+    FROM employee, role, department 
+    WHERE department.id = role.department_id 
+    AND role.id = employee.role_id
+    ORDER BY employee.id ASC`;
+  connection.query(sql, (error, response) => {
+    if (error) throw error;
+    console.log(`Current Employees:`);
+    console.table(response);
+    promptUser();
+  });
+};
